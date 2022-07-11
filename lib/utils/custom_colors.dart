4d057cd982fd67e55d57a98f347cea7969d1d584
Ihor986 <235689ks@gmail.com>
2022-07-11 13:21:18 +0300

    update colors

--- lib/utils/custom_colors.dart
+++ lib/utils/custom_colors.dart
@@ -3,34 +3,30 @@ import 'package:flutter/material.dart';
 class CustomColors {
   CustomColors._();
 
-  static const Color blueSoso = Color.fromRGBO(140, 132, 226, 1);
-  static const Color oliveSoso = Color.fromRGBO(113, 165, 159, 1);
-  static const Color oliveSosoOp = Color.fromRGBO(113, 165, 159, 0.85);
-  static const Color rose = Color.fromRGBO(241, 180, 136, 1);
-  static const Color roseOp = Color.fromRGBO(241, 180, 136, 0.85);
-  static const Color white = Color.fromRGBO(246, 246, 246, 1);
-  static const Color whiteOp = Color.fromRGBO(246, 246, 246, 0.9);
-  static const Color whiteOp08 = Color.fromRGBO(255, 255, 255, 0.8);
-  static const Color invisible = Color.fromRGBO(246, 246, 246, 0);
-  static const Color whiteOp05 = Color.fromRGBO(246, 246, 246, 0.5);
-  static const Color boxShadow = Color.fromRGBO(0, 0, 0, 0.11);
-  static const Color black = Color.fromRGBO(58, 58, 58, 1);
-  static const Color greenTalesSelection = Color.fromRGBO(113, 165, 159, 1);
-  static const Color iconsColorBNB = Color.fromRGBO(58, 58, 85, 0.8);
-  static const Color noTalesText = Color.fromRGBO(58, 58, 85, 0.5);
-  static const Color audioBorder = Color.fromRGBO(58, 58, 85, 0.2);
-  static const Color openAllAudio = Color.fromRGBO(58, 58, 85, 1);
-  static const Color red = Color.fromRGBO(226, 119, 119, 1);
-  static const Color iconsColorPlayRecUpbar = Color.fromRGBO(95, 95, 117, 1);
-  static const Color audiotalesHeadColorBlue = Color.fromRGBO(94, 119, 206, 1);
-  static const Color audiotalesBlue = Color.fromRGBO(103, 139, 210, 0.85);
-  static const Color playAllButtonActiveRepeat =
-      Color.fromRGBO(246, 246, 246, 0.2);
-  static const Color playAllButtonDisactiveRepeat =
-      Color.fromRGBO(246, 246, 246, 0.5);
-  static const Color playAllButtonDisactive =
-      Color.fromRGBO(246, 246, 246, 0.16);
-  static const Color disactive = Color.fromRGBO(229, 229, 229, 0.4);
-  static const Color hudenBackground = Color.fromRGBO(0, 0, 0, 0.6);
-  static const Color deleteWarningTextColor = Color.fromRGBO(58, 58, 85, 0.7);
+  static const Color blueSoso = Color(0xFF8C84E2);
+  static const Color oliveSoso = Color(0xFF71A59F);
+  static const Color oliveSosoOp = Color(0xD771A59F);
+  static const Color rose = Color(0xFFF1B488);
+  static const Color roseOp = Color(0xD7F1B488);
+  static const Color white = Color(0xFFF6F6F6);
+  static const Color whiteOp = Color(0xE3F6F6F6);
+  static const Color whiteOp08 = Color(0xCCFFFFFF);
+  static const Color invisible = Color(0x00F6F6F6);
+  static const Color whiteOp05 = Color(0x7EF6F6F6);
+  static const Color boxShadow = Color(0x1C000000);
+  static const Color black = Color(0xFF3A3A3A);
+  static const Color iconsColorBNB = Color(0xCC3A3A55);
+  static const Color noTalesText = Color(0x7E3A3A55);
+  static const Color audioBorder = Color(0x333A3A55);
+  static const Color openAllAudio = Color(0xFF3A3A55);
+  static const Color red = Color(0xFFE27777);
+  static const Color iconsColorPlayRecUpbar = Color(0xFF5F5F75);
+  static const Color audiotalesHeadColorBlue = Color(0xFF5E77CE);
+  static const Color audiotalesBlue = Color(0xD7678BD2);
+  static const Color playAllButtonActiveRepeat = Color(0x33F6F6F6);
+  static const Color playAllButtonDisactiveRepeat = Color(0x7EF6F6F6);
+  static const Color playAllButtonDisactive = Color(0x28F6F6F6);
+  static const Color disactive = Color(0x66E5E5E5);
+  static const Color hudenBackground = Color(0x99000000);
+  static const Color deleteWarningTextColor = Color(0xB13A3A55);
 }
